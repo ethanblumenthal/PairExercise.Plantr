7f8d2bb861ff95727d02a90329f2b26aa6530e83
Alexandra Gerard <alexandralbgerard@gmail.com>
2019-01-22 16:23:57 -0600

    data is seeding into database, no relationships created yet

--- models.js
+++ models.js
@@ -3,26 +3,31 @@ const db = new Sequelize('postgres://localhost:5432/plantr');
 
 const Gardener = db.define('gardener', {
   name: Sequelize.STRING,
-  age: Sequelize.INTEGER
+  age: Sequelize.INTEGER,
 });
 
 const Plot = db.define('plot', {
   size: Sequelize.INTEGER,
-  shaded: Sequelize.BOOLEAN
+  shaded: Sequelize.BOOLEAN,
 });
 
 const Vegetable = db.define('vegetable', {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
-  planted_on: Sequelize.DATE
+  planted_on: Sequelize.DATE,
 });
 
 Plot.belongsTo(Gardener);
 Gardener.hasOne(Plot);
 
-Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
-Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
+Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
+Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
 
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 
-module.exports = db;
+module.exports = {
+  db,
+  Gardener,
+  Plot,
+  Vegetable,
+};
--- seed.js
+++ seed.js
@@ -1,12 +1,49 @@
-const db = require('./models');
+const { db, Gardener, Plot, Vegetable } = require('./models');
 
-db.sync({force: true})
+const vegetablesData = [
+  { name: 'beets', color: 'purple', planted_on: Date.now() },
+  { name: 'cucumbers', color: 'green', planted_on: Date.now() },
+  { name: 'carrots', color: 'orange', planted_on: Date.now() },
+  { name: 'tomatoes', color: 'red', planted_on: Date.now() },
+];
+
+const gardenerData = [
+  { name: 'gerry', age: 24 },
+  { name: 'ethan', age: 25 },
+  { name: 'anna', age: 32 },
+];
+
+const plotData = [
+  { size: 40, shaded: true },
+  { size: 47, shaded: false },
+  { size: 100, shaded: false },
+];
+
+db.sync({ force: true })
   .then(() => {
     console.log('Database synced!');
+    const promiseForVeggies = Vegetable.bulkCreate(vegetablesData, {
+      returning: true,
+    });
+    const promiseForGardener = Gardener.bulkCreate(gardenerData, {
+      returning: true,
+    });
+    const promiseForPlot = Plot.bulkCreate(plotData, {
+      returning: true,
+    });
+    return Promise.all([promiseForVeggies, promiseForGardener, promiseForPlot]);
+  })
+  .then(insertedData => {
+    const [veggies, gardeners, plots] = insertedData;
+    const [beets, cucumbers, carrots, tomatoes] = veggies;
+    const [gerry, ethan, anna] = gardeners;
+    const [one, two, three] = plots;
+
+    console.log(Object.keys(veggies.__proto__));
   })
   .catch(err => {
     console.log(err);
   })
   .finally(() => {
     db.close();
-});
+  });
